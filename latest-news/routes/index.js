9a4d540f2d4d44739dd7663031576ad592ce5acf
Alyssa <me@alyssamendoza.com>
2016-03-21 19:59:43 -0700

    Added upvote methods

--- latest-news/routes/index.js
+++ latest-news/routes/index.js
@@ -1,9 +1,100 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET posts in routes/index.js */
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* return a single post */
+router.param('post', function(req, res, next) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+/* return comment */
+router.param('comment', function(req, res, next) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(req.post);  
+  });
+});
+
+/* route to save upvotes */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* route for comments for a particular post */
+router.put('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, post){
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/* comment upvotes */
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) { return next(err); }
+    res.json(comment);
+  });
 });
 
 module.exports = router;
